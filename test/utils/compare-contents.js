bcfefdfbecd29c253f4e88120de6adb9d25ac6a5
James Messinger <james.messinger@gmail.com>
2019-08-25 13:03:22 -0500

    Improved the `compareContents()` helper function to compare small chunks at a time rather than the whole file.  This makes it easier to spot differences

--- test/utils/compare-contents.js
+++ test/utils/compare-contents.js
@@ -10,5 +10,28 @@ module.exports = compareContents;
  */
 async function compareContents (actualContents, fileName) {
   let expectedContents = await readFile(`test/fixtures/modified/${fileName}`, "utf8");
-  expect(actualContents).to.equal(expectedContents);
+
+  // Split both files into separate lines
+  let actualLines = actualContents.split("\n").filter(Boolean);
+  let expectedLines = expectedContents.split("\n").filter(Boolean);
+
+  let lineCount = Math.max(actualLines.length, expectedLines.length);
+  let chunkSize = 50;
+
+  // Compare each line
+  for (let i = 0; i < lineCount; i++) {
+    let actual = actualLines[i];
+    let expected = expectedLines[i];
+
+    // Compare the lines in small chunks, since inlined SVGs can be huuuuuge,
+    // which makes it hard to see the differences when there are mismatches
+    let offset = 0;
+    while (actual.length > offset || expected.length > offset) {
+      let actaulChunk = actual.slice(offset, offset + chunkSize);
+      let expectedChunk = expected.slice(offset, offset + chunkSize);
+
+      expect(actaulChunk).to.equal(expectedChunk, `Line #${i + 1} does not match line #${i + 1} of ${fileName}`);
+      offset += chunkSize;
+    }
+  }
 }
